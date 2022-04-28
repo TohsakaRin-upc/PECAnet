970b15a1d9ea60562ef60cf336113a01fc5cb563
TohsakaRin_upc <57391048+TohsakaRin-upc@users.noreply.github.com>
2022-04-28 17:43:26 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+##Save path of weight file
+TRAINED_MODEL = '/hy-tmp/PECAnet/save_model'
+
+#Storage location of data set
+TRAIN_DATASET_DIR = '/hy-tmp/stcars/trainval'
+
+
+VALID_DATASET_DIR = '/hy-tmp/stcars/test'
+
+
+DATASET_DIR='/hy-tmp'
+
+
+modelzoopath='/hy-tmp/PECAnet/pth_model'
